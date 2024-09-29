f1935fb475388a95e286c3b98683ec603103396c
NikhTheTech <95464996+NikhTheTech@users.noreply.github.com>
2024-09-30 02:46:48 +0530

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -1,27 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-
 contract ERC20 {
+
+
     uint public totalSupply;
     mapping(address => uint) public balanceOf;
-    mapping(address => mapping(address => uint)) public allowance;
+    mapping(address => mapping(address => uint)) public allowance; 
+    
     string public name = "NikhTheTech";
     string public symbol = "NTT";
-    uint8 public decimals = 1;
+    uint8 public decimals = 18;
 
-    event Transfer(address indexed from, address indexed to, uint value);
+	event Transfer(address indexed from, address indexed to, uint value);
     event Approval(address indexed owner, address indexed spender, uint value);
 
     function transfer(address recipient, uint amount) external returns (bool) {
-        balanceOf[msg.sender] -= amount;
+        balanceOf[msg.sender] -= amount; // this line isn't the problem
         balanceOf[recipient] += amount;
         emit Transfer(msg.sender, recipient, amount);
         return true;
     }
 
-    function approve(address spender, uint amount) external returns (bool) {
-        allowance[msg.sender][spender] = amount;
-        emit Approval(msg.sender, spender, amount);
+    function approve(address owner, address spender, uint amount) external returns (bool) {
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
         return true;
     }
 
